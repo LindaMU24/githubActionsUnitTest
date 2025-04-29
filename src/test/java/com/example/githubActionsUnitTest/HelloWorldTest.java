453f7538a1239531d542ffc2a509db6822996e7e
Linda Blomkvist <3tomu24.blomli@folkuniversitetet.nu>
2025-04-29 15:07:22 +0200

    added new JUnit test

---
+++ src/test/java/com/example/githubActionsUnitTest/HelloWorldTest.java
@@ -0,0 +1,12 @@
+package com.example.githubActionsUnitTest;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class HelloWorldTest {
+    @Test
+    void helloWorldTest() {
+        assertTrue(true);
+    }
+}
